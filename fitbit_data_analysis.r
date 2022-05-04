ba40022dfd7560612d5e6c8db630c7f5ba5ce21c
scaprisecca <50277797+scaprisecca@users.noreply.github.com>
2022-05-04 05:30:16 -0700

    reformatted date/time values in datasets

--- fitbit_data_analysis.r
+++ fitbit_data_analysis.r
@@ -8,10 +8,10 @@ library(msgr)
 
 # Load the data
 
-activity_df <- read_csv("~/Documents/bellabeat_case_study/fitbit_data_original/dailyActivity_merged.csv")
-hr_df <- read_csv("~/Documents/bellabeat_case_study/fitbit_data_original/heartrate_seconds_merged.csv")
-sleep_df <- read_csv("~/Documents/bellabeat_case_study/fitbit_data_original/sleepDay_merged.csv")
-weightlog_df <- read_csv("~/Documents/bellabeat_case_study/fitbit_data_original/weightLogInfo_merged.csv")
+activity_df <- read_csv("~/Documents/my_docs/bellabeat_case_study/fitbit_data_original/dailyActivity_merged.csv")
+hr_df <- read_csv("~/Documents/my_docs/bellabeat_case_study/fitbit_data_original/heartrate_seconds_merged.csv")
+sleep_df <- read_csv("~/Documents/my_docs/bellabeat_case_study/fitbit_data_original/sleepDay_merged.csv")
+weightlog_df <- read_csv("~/Documents/my_docs/bellabeat_case_study/fitbit_data_original/weightLogInfo_merged.csv")
 
 # Checking on data types of each dataset
 
@@ -26,14 +26,17 @@ glimpse(weightlog_df)
 #  mutate(Time = dmy_hms(Time)) %>% 
 #  separate(Time, into = c('Date', 'Time'), sep = ' ')
 
+
+hr_df$Time <- parse_date_time(hr_df$Time, orders = "%m/%d/%Y %I:%M:%S %p")
 hr_df$Date <- format(as.Date(hr_df$Time, format = "%m/%d/%Y"), "%m/%d/%Y")
-hr_df$time <- format(as.POSIXct(hr_df$Time, format = "%d/%m/%Y %I:%M:%S %p"), "%I:%M:%S %p")
+hr_df$Time <- format(as.POSIXct(hr_df$Time, format = "%m/%d/%Y %I:%M:%S %p"), "%I:%M:%S %p")
 hr_df <- hr_df %>% 
   select(Id, Date, Time, Value)
 
-
-h2$test <- parse_date_time(h2$Time, orders = "%m/%d/%Y %I:%M:%S %p")
-h2$Time2 <- format(as.POSIXct(h2$Time, format = "%m/%d/%Y %I:%M:%S %p"), "%I:%M:%S %p")
+sleep_df$SleepDay <- parse_date_time(sleep_df$SleepDay, orders = "%m/%d/%Y %I:%M:%S %p")
+sleep_df$Date <- format(as.Date(sleep_df$SleepDay, format = "%m/%d/%Y"), "%m/%d/%Y")
+sleep_df <- sleep_df %>% 
+  select(Id, Date, TotalSleepRecords, TotalMinutesAsleep, TotalTimeInBed)
 
 # Checking for the number of users in each dataset
 
